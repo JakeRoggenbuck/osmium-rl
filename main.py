c5ad70f5e4bcd2fae66e81f9c1dd4c9e79139e56
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-12-09 13:14:44 -0800

    Add player and colors

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame
 
+
+class ColorScheme:
+    grey_background = pygame.Color("#212F3C")
+    player_red = pygame.Color("#E3301C")
+
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -14,13 +24,25 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill(ColorScheme.grey_background)
 
-    # RENDER YOUR GAME HERE
+    pygame.draw.circle(screen, ColorScheme.player_red, player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
+
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
